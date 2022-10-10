16ee4a733530b68e8936adedf018785fb2d937a4
DJMIKE20 <m.zimm20@gmail.com>
2022-10-10 15:31:46 -0400

    added login and logout routes and html and js

--- server.js
+++ server.js
@@ -4,17 +4,35 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs  = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// session setup object
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db:sequelize
+    })
+};
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// app use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
+
+// handlebars engine and setup
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 // turn on routes
 app.use(routes);
 
