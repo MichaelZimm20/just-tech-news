86a6b8ea4ed0c7f0ae937b97c54c24cc5fa16b39
DJMIKE20 <m.zimm20@gmail.com>
2022-10-11 15:30:32 -0400

    added login and logout functionality and create a single post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -88,13 +88,15 @@ router.post('/', (req,res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    //custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+    if (req.session) {
+        //custom static method created in models/Post.js
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedPostData => res.json(updatedPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
         });
+    }
 });
 
 // update a Post's title
